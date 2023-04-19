f27bce8b4996d388af46b35b9d7393db1c95e6aa
Nicolas Felipe Vanderstichel <nfvander@gmail.com>
2023-04-19 11:44:20 -0300

    Cambios en el login

--- js/main.js
+++ js/main.js
@@ -4,11 +4,13 @@ const register = document.getElementById("register");
 const formularios = document.getElementById("formularios");
 const iniciarsesion = document.getElementById("iniciar-sesion");
 const iniciarRegistro = document.getElementById("iniciar-registro");
+const cajaTrasera__loginContenedor =document.getElementById("caja-trasera__login-contenedor")
 
 const navegador = document.getElementById("navegador")
 const carousel = document.getElementById("carousel")
 const separador_del_medio = document.getElementById("separador_del_medio")
 const articulos=document.getElementById("articulos")
+const donde_estamos=document.getElementById("donde-estamos")
 const formulario_de_contacto = document.getElementById("formulario_de_contacto")
 const footer = document.getElementById("footer")
 
@@ -20,6 +22,7 @@ function iniciarLogin() {
     navegador.style.display = "none"
     carousel.style.display = "none"
     separador_del_medio.style.display = "none"
+    donde_estamos.style.display="none"
     articulos.style.display="none"
     formulario_de_contacto.style.display = "none"
     footer.style.display = "none"
@@ -33,6 +36,7 @@ function finalizarLogin() {
         navegador.style.display = "block"
         carousel.style.display = "block"
         separador_del_medio.style.display = "block"
+        donde_estamos.style.display="flex"
         articulos.style.display="flex"        
         formulario_de_contacto.style.display = "flex"
         footer.style.display = "block"
@@ -54,6 +58,8 @@ function registrarse() {
         login.style.display = "none"
         iniciarsesion.style.opacity = "1"
         iniciarRegistro.style.opacity = "0"
+        cajaTrasera__loginContenedor.style.opacity="1"
+
     }
     else {
         register.style.display = "block"
@@ -61,6 +67,7 @@ function registrarse() {
         login.style.display = "none"
         iniciarsesion.style.opacity = "1"
         iniciarRegistro.style.opacity = "0"
+        cajaTrasera__loginContenedor.style.opacity="1"
     }
 }
 function iniciaSesion() {
@@ -76,3 +83,32 @@ function iniciaSesion() {
     iniciarsesion.style.opacity = "0"
     iniciarRegistro.style.opacity = "1"
 }
+
+// validacion de formulario de contacto
+const nombre=document.getElementById("nombre");
+const email=document.getElementById("email");
+const form=document.querySelector(".formulario");
+const label_nombre=document.getElementById("label_nombre")
+const label_email=document.getElementById("label_email")
+
+form.addEventListener("submit",e=>{
+    let advertencia=""
+    let regexEmail=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    let entrar=false
+    if (nombre.value.legth<1) {
+        advertencia+="Debe ingresar un nombre <br>" 
+        entrar=true
+        
+    }
+    if(!regexEmail.test(email.value)){
+        advertencia+="el email que ingreso no es correcto"
+        entrar=true
+console.log("asd");
+    }
+    if(entrar){
+        label_nombre.innerHTML=advertencia
+        label_email.innerHTML=advertencia
+
+    }
+
+})
