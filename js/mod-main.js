51a378d2449240e950bb77eb66d42ba1c21d34ce
Nicolas Felipe Vanderstichel <nfvander@gmail.com>
2023-04-19 21:07:03 -0300

    añade pdf proyecto, ultimación detalles css y arreglo de MediaQueries

--- js/mod-main.js
+++ js/mod-main.js
@@ -261,4 +261,75 @@ chartBtn.onclick = function () {
 }
 
 
+/////////////////////////////////////////////////////////////////////
+// Codigo Wizzard register 
+
+//your javascript goes here
+var currentTab = 0;
+document.addEventListener("DOMContentLoaded", function(event) {
+
+
+    showTab(currentTab);
+    const form = document.getElementById("regForm");
+    form.addEventListener('submit', (event) => { event.preventDefault(); })
+});
+
+function showTab(n) {
+    var x = document.getElementsByClassName("tab");
+    x[n].style.display = "block";
+    if (n == 0) {
+        document.getElementById("prevBtn").style.display = "none";
+    } else {
+        document.getElementById("prevBtn").style.display = "inline";
+    }
+    if (n == (x.length - 1)) {
+        document.getElementById("nextBtn").innerHTML = "Registrar";
+    } else {
+        document.getElementById("nextBtn").innerHTML = "Siguiente";
+    }
+    fixStepIndicator(n)
+   
+}
+
+function nextPrev(n) {
+    var x = document.getElementsByClassName("tab");
+    if (n == 1 && !validateForm()) return false;
+    x[currentTab].style.display = "none";
+    currentTab = currentTab + n;
+    if (currentTab >= x.length) {
+        
+        console.log(document.getElementById("nextprevious").style.display)
+
+        document.getElementById("nextprevious").style.display = "none";
+        document.getElementById("nextprevious").classList.value = "";
+        document.getElementById("all-steps").style.display = "none";
+        document.getElementById("registerH1").style.display = "none";
+        document.getElementById("text-message").style.display = "block";
+        
+        
+
+
+    }
+    showTab(currentTab);
+}
+
+function validateForm() {
+    var x, y, i, valid = true;
+    x = document.getElementsByClassName("tab");
+    y = x[currentTab].getElementsByTagName("input");
+    for (i = 0; i < y.length; i++) {
+        if (y[i].value == "") {
+            y[i].className += " invalid";
+            valid = false;
+        }
+    }
+    if (valid) { document.getElementsByClassName("step")[currentTab].className += " finish"; }
+    return valid;
+}
+
+function fixStepIndicator(n) {
+    var i, x = document.getElementsByClassName("step");
+    for (i = 0; i < x.length; i++) { x[i].className = x[i].className.replace(" active", ""); }
+    x[n].className += " active";
+}
 
